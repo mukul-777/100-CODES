5da261e345c8417a275e62a9354add3e14c1e859
mukul-777 <mukulshrivastav11@gmail.com>
2025-09-04 20:57:11 +0530

    Add files via upload

---
+++ Day 1.1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter fist number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Sum of %d and %d is %d",a,b,c);
+    return 0;
+}
